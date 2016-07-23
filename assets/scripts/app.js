b062d181a87afe9ab5c1652a0f8c4bf1d960469b
Alexander Glassford <a.b.glassford@gmail.com>
2016-07-23 16:11:23 -0600

    add dynamic picture selection

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,20 @@
 // Your code here!
-$(function(){
-  
+$(function () {
+  $('form').on('submit', function (event) {
+    event.preventDefault()
+  })
+  $.ajax({
+    method: 'GET',
+    url: 'http://galvanize-student-apis.herokuapp.com/gpersonnel/roles'
+  }).done(function (data) {
+    for (var i = 1; i <= 4; i++) {
+      $('#op' + i).text(data[i - 1].title)
+      $('#op' + i).attr('data-url', data[i - 1].img)
+    }
+    $('#roles').on('change', function () {
+      var url = $('option:selected').attr('data-url')
+      $('#rolePic').attr('src', url)
+    })
+
+  })
 })
